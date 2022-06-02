60a6772f874d896418fd14ea7f312a7c61459c77
awarbler <awoodfor@byui.edu>
2022-06-02 13:41:21 -0500

    tryin heroku

--- flash/flash/settings.py
+++ flash/flash/settings.py
@@ -20,7 +20,7 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -128,7 +128,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS  = (os.path.join(BASE_DIR,'static'),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
